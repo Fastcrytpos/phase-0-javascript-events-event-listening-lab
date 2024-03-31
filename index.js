e99daf7f3129bbb0e3f0055ca157d7e252f6f1d9
Samson Mubeya <rasspartah@gmail.com>
2024-04-01 00:09:25 +0300

    did my assignment

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 function addingEventListener() {
+    let btn =document.getElementById("button")
+    btn.addEventListener("click",addaddiv);
+    console.log("button")
+    
+
 }
+let x=0;
+    
+function addaddiv(){
+    let divs=document.getElementsByTagName("div")
+    
+    let div6=document.createElement("div")
+    x++;
+    lastd=divs.length-1;
+    lastdiv=divs[lastd]
+    b=lastdiv.textContent;
+    div6.textContent = x+b;
+    divs[divs.length-1].append(div6);
+    console.log(x)
+
+}
+
+addingEventListener();
